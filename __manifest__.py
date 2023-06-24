cbc59428cee2c6af73dc8dec7f2c5618157f3796
muhmmed-mehsen <137640375+muhmmed-mehsen@users.noreply.github.com>
2023-06-24 20:39:07 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Covenent_Record_menagment',
+    'version': '1.0.0',
+    'sequence': -100,
+    'author': 'muhmmed msalim',
+    'website': ['www.msalim','.com'],
+    'category': 'Covenents',
+    'summary': 'Covenents managment system',
+    'description': 'Covvenents management system',
+    'depends': ['base'],
+    'data': [
+        'views/Covenet_record_views.xml',
+        'security/ir.model.access.csv',
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3'
+}
